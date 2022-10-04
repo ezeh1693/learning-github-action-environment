5d970fc2626c6b75b6e8c803c4a755f3b5ab5342
Ezeh Emmanuel <ezeh1693@gmail.com>
2022-10-04 08:48:03 +0100

    Minor change

---
+++ echo-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "🎉 The job was automatically triggered by an event."
+echo "Showing ANDELA_ID_SECRET: "
+echo ${ ANDELA_ID_SECRET}
+echo "Showing GOOGLE_ANDELA_CLIENT_SECRET: "
+echo ${ GOOGLE_ANDELA_CLIENT_SECRET}
